c58cbf1ccd392ccefd03644957286caab1c9a2f2
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-01-01 09:34:32 +0700

    Add option --fields (-f) to select which fields to return

--- lib/App/MetaCPANUtils.pm
+++ lib/App/MetaCPANUtils.pm
@@ -11,6 +11,47 @@ use warnings;
 
 our %SPEC;
 
+our $release_fields = [qw/
+                             release
+                             date
+                             author
+                             maturity
+                             version
+                             distribution
+                             abstract
+/];
+our %argopt_release_fields = (
+    fields => {
+        schema => ['array*', of=>['str*', in=>$release_fields]],
+        default => $release_fields,
+        cmdline_aliases=>{f=>{}},
+        tags => ['category:filtering'],
+    },
+);
+
+sub _resultset_to_envres {
+    my ($resultset, $wanted_fields) = @_;
+
+    my @rows;
+    my $resmeta = {'table.fields' => $wanted_fields};
+    while (my $obj = $resultset->next) {
+        my $row = {};
+        if (ref $obj eq 'MetaCPAN::Client::Release') {
+            $row->{release}      = $obj->name         if grep {$_ eq 'release'}      @$wanted_fields;
+            $row->{date}         = $obj->date         if grep {$_ eq 'date'}         @$wanted_fields;
+            $row->{author}       = $obj->author       if grep {$_ eq 'author'}       @$wanted_fields;
+            $row->{maturity}     = $obj->maturity     if grep {$_ eq 'maturity'}     @$wanted_fields;
+            $row->{version}      = $obj->version      if grep {$_ eq 'version'}      @$wanted_fields;
+            $row->{distribution} = $obj->distribution if grep {$_ eq 'distribution'} @$wanted_fields;
+            $row->{abstract}     = $obj->abstract     if grep {$_ eq 'abstract'}     @$wanted_fields;
+        } else {
+            die "Can't handle result $obj";
+        }
+        push @rows, $row;
+    }
+    [200, "OK", \@rows, $resmeta];
+}
+
 $SPEC{list_recent_metacpan_releases} = {
     v => 1.1,
     args => {
@@ -24,6 +65,7 @@ $SPEC{list_recent_metacpan_releases} = {
 #_
             pos => 0,
         },
+        %argopt_release_fields,
     },
 };
 sub list_recent_metacpan_releases {
@@ -33,20 +75,7 @@ sub list_recent_metacpan_releases {
 
     my $mcpan = MetaCPAN::Client->new;
     my $recent = $mcpan->recent($args{n});
-    my @rows;
-    while (my $rel = $recent->next) {
-        push @rows, {
-            release      => $rel->name,
-            date         => $rel->date,
-            author       => $rel->author,
-            maturity     => $rel->maturity,
-            version      => $rel->version,
-            distribution => $rel->distribution,
-            abstract     => $rel->abstract,
-        };
-    }
-
-    [200, "OK", \@rows];
+    _resultset_to_envres($recent, $args{fields});
 }
 
 1;
